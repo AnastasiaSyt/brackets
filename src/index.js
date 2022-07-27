7858315aa1a628f0ec3578642d69cddacbdccdfa
Anastasia Sytsevich <drozdushka@gmail.com>
2022-07-27 20:17:19 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const bracketsOpen = ['(', '[', '{', '|', '1', '3', '5', '7', '8'];
+  const pairsBrackets = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['|']: '|',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+    ['7']: '7',
+    ['8']: '8'
+  };
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const symbolN = str[i];
+
+    if (stack.length !== 0) {
+      const topElement = stack[stack.length - 1];
+      if (pairsBrackets[symbolN] === topElement) {
+        stack.pop();
+        continue;
+      }
+    }
+
+    if (bracketsOpen.includes(symbolN)) {
+      stack.push(symbolN);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length -1];
+
+      if (pairsBrackets[symbolN] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+
+    }
+  }
+  return stack.length === 0;
 }
